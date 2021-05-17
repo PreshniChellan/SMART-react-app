958852b047c10b9b414a92ca134163712eb6adbc
Danni <DanielleNyxDaSilva@gmail.com>
2021-05-17 15:35:05 +0200

    Added Login page and new dependancies

--- smart-react-app/src/Pages/Sales.js
+++ smart-react-app/src/Pages/Sales.js
@@ -1,5 +1,10 @@
+import { Redirect } from 'react-router-dom'
 
-export default function Sales() {
+export default function Sales({ authorized }) {
+
+    if (!authorized) {
+        return <Redirect to='/' />
+    }
 
     return (
         <div>

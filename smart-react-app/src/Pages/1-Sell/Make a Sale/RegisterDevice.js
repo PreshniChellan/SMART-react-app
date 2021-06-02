6a42b8816d11b4a485318b8f5434e1948e171627
Nyxerian <preshnim@newera.agency>
2021-06-02 13:04:16 +0200

    Added register device pages

---
+++ smart-react-app/src/Pages/1-Sell/Make a Sale/RegisterDevice.js
@@ -0,0 +1,13 @@
+
+
+export default function RegisterDevice() {
+
+
+    return (
+        <>
+            <p>Register Device</p>
+
+            
+        </>
+    )
+}
--- smart-react-app/src/Pages/2-Dashboards/Sales/Sales.js
+++ smart-react-app/src/Pages/2-Dashboards/Sales/Sales.js
@@ -5,6 +5,8 @@ import Sidebar from '../../../Components/SideNav/Sidebar'
 import GenButton from '../../../Components/Buttons/GenButton'
 
 
+
+
 export default function Sales() {
 
     let history = useHistory(); 
@@ -13,17 +15,34 @@ export default function Sales() {
         history.push("/")
     }
 
+    
+
     return (
         <div>
             <Sidebar />
             <div className='content'>
                 <h2>Sales Page</h2>
+                <div className='buttons'>
                 <GenButton 
-                    value="Sales"
+                    value="Button 1"
                     onClick={handleRoute}
-                />
+                />{' '}
+                <GenButton 
+                    value="Button 2"
+                    onClick={handleRoute}
+                />           
+                </div>
             
             </div>
+            <br />
+            <br />
+            <br />
+
+            
+
+            
+
+
         </div>
     )
 }

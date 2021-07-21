bfc5c259f0654d0aca7ba0eee86e141b38f18c92
Nyxerian <DanielleNyxDaSilva@gmail.com>
2021-07-21 10:19:26 +0200

    Creation of Referrals page

--- smart-react-app/src/Pages/5-Leads/Referrals/Referrals.js
+++ smart-react-app/src/Pages/5-Leads/Referrals/Referrals.js
@@ -1,6 +1,8 @@
 import Sidebar from '../../../Components/SideNav/Sidebar'
 import '../Leads.css'
 
+import LoadRef from '../Leads Components/LoadRef'
+
 
 export default function Referrals() {
 
@@ -8,7 +10,7 @@ export default function Referrals() {
         <div>
             <Sidebar />
             <br />
-            <h2 className='headerTitle'>Referrals Page</h2>
+            <LoadRef />
         </div>
     )
 }
